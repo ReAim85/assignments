20bedbe235d2e71c0b9a246469d1d58c46ab3da9
Vishal Jha <visual19852004@gmail.com>
2024-10-08 17:22:28 +0530

    middleware assignment completed

--- week-4/middlewares/02-logIncomingRequests.js
+++ week-4/middlewares/02-logIncomingRequests.js
@@ -5,6 +5,14 @@ const app = express();
 
 function logRequests(req, res, next) {
     // write the logic for request log here
+    const method = req.method;
+    const url = req.originalUrl;
+    const timeStamp = new Date().toISOString(); 
+    if(method === 'GET' || method === 'POST') {
+        console.log(`${method} ${url} - ${timeStamp}`);
+    }
+    next();
+
 }
 
 app.use(logRequests);
@@ -12,5 +20,10 @@ app.use(logRequests);
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Hello, world!' });
 });
+
+app.post('/', (req, res) => {
+    res.status(404).send('not found ')
+})
+app.listen(3000)
 
 module.exports = app;
